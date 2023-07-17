288512264132a4f41b5c5b8631312221c8322952
sy3kic <sy3kic@users.noreply.github.com>
2023-07-17 16:57:38 +0100

    manual autoscale to 2

--- main.tf
+++ main.tf
@@ -36,8 +36,9 @@ module "autoscaling" {
   version = "6.10.0"
   
   name = "blog"
-  min_size = 1
-  max_size = 2
+  min_size         = 1
+  max_size         = 2
+  desired_capacity = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
