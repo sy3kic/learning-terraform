952cb57b832156ae8b36f4e59a259341082d6219
sy3kic <sy3kic@users.noreply.github.com>
2023-07-17 16:51:10 +0100

    autoscaling

--- main.tf
+++ main.tf
@@ -31,19 +31,23 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.10.0"
+  
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_idnetifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sq.security_group_id]
+
+  image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
-  subnet_id              = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sq.security_group_id]
-
-  tags = {
-    Name = "HelloWorld"
-  }
 }
 
-module "blog-alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
